ef1183cac212f47d3427fd38be45686fc7b93757
kumagai <22csc0005@edu.sca.ac.jp>
2023-10-21 18:14:08 +0900

    スキルスクリプトの追加

---
+++ Assets/kumagai/Scripts/SkillSelection.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillSelection : MonoBehaviour
+{
+    private GameObject nowChar;
+    private string[] skillName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerMoveGage.MoveChar[0]!=null)
+        {
+
+        }
+    }
+}
