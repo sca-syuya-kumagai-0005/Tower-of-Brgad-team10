ccc6661307c0f4b01036c74d097ab0b5311a9ed5
Fukaya <sdt22cge0018@edu.sca.ac.jp>
2023-10-21 17:38:28 +0900

    シーン移動完成

--- Assets/Fukaya/Scripts/ChangeScene.cs
+++ Assets/Fukaya/Scripts/ChangeScene.cs
@@ -5,16 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    //キャラ選択へ
     public void OnClickStartButton()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("CharaSelectScene");
     }
+
+    //キャラ選択へ戻る
+    public void OnClickCSBackButton()
+    {
+        SceneManager.LoadScene("CharaSelectScene");
+    }
+
+
+    //ランキングへ
     public void OnClickRankingButton()
     {
         SceneManager.LoadScene("RankingScene");
     }
+
+    //設定へ
     public void OnClickOptionButton()
     {
         SceneManager.LoadScene("OptionScene");
+    }
+
+    //タイトルに戻る
+    public void OnClickBackButton()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    //ステージ選択へ
+    public void OnClickOStageSelectButton()
+    {
+        SceneManager.LoadScene("StageSelectScene");
     }
 }
