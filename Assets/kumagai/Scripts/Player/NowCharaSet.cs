8e299d744ca282226958dd0389d2dcaf8cbdb6cf
熊谷柊哉 <sdt22csc0005@edu.sca.ac.jp>
2023-12-20 15:47:27 +0900

    NowキャラNextキャラの調整

--- Assets/kumagai/Scripts/Player/NowCharaSet.cs
+++ Assets/kumagai/Scripts/Player/NowCharaSet.cs
@@ -14,12 +14,15 @@ public class NowCharaSet : MonoBehaviour
     private GameObject MoveChara;
     [SerializeField]
     private GameObject nextCharaSponePos;
+    public static GameObject nextChara;
+    public static GameObject nowChara;
     // Start is called before the first frame update
     void Start()
     {
         for(int i=0;i<PlayerEditor.PlayerName.Length;i++) {
             GameObject now=Resources.Load<GameObject>("NowChara/"+PlayerEditor.PlayerName[i]);
             GameObject next=Resources.Load<GameObject>("NextChara/"+PlayerEditor.PlayerName[i]);
+            Debug.Log(next.name);
             nowCharacter.Add(Instantiate(now,nowCharaSponePos.transform.position,Quaternion.identity,MoveChara.transform));
             nextCharacter.Add(Instantiate(next,nextCharaSponePos.transform.position,Quaternion.identity,MoveChara.transform));
           
@@ -34,22 +37,26 @@ public class NowCharaSet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(SkillStorage.nextCharaNumber);
-        for(int i=0;i<PlayerEditor.PlayerName.Length;i++) 
-        {
-            if(SkillStorage.charaNumber==i) 
+        if(GameManager.BattleState.start!=GameManager.BattleState.flagReSet)
+        { 
+            for(int i=0;i<PlayerEditor.PlayerName.Length;i++) 
             {
-                nowCharacter[i].SetActive(true);
-            } 
-            if(CharaMoveGage.MoveChar[0]==null)
-            {
-                nowCharacter[i].SetActive(false);
-            }
+                if(SkillStorage.charaNumber==i) 
+                {
+                    nowCharacter[i].SetActive(true);
+                    nowChara =nowCharacter[i];
+                } 
+                if(CharaMoveGage.MoveChar[0]==null)
+                {
+                    nowCharacter[i].SetActive(false);
+                }
 
-            if(SkillStorage.nextCharaNumber==i)
-            {
-                nextCharacter[i].SetActive(true);
-                Debug.Log("’Ê‰ß‚µ‚Ä‚¢‚é‚æ");
+                if(SkillStorage.nextCharaNumber==i)
+                {
+                    nextCharacter[i].SetActive(true);
+                    nextChara=nextCharacter[i];
+                    Debug.Log("’Ê‰ß‚µ‚Ä‚¢‚é‚æ");
+                }
             }
         }
       
