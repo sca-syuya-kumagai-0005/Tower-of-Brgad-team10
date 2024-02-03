f1647d62ba0fb8f3aa55c6a7bffa7de73b4c192d
熊谷柊哉 <sdt22csc0005@edu.sca.ac.jp>
2024-02-03 16:45:57 +0900

    バフデバフのアイコンを生成する基本システムを実装

--- Assets/kumagai/Scripts/BattleScene/Twinkle.cs
+++ Assets/kumagai/Scripts/BattleScene/Twinkle.cs
@@ -5,9 +5,8 @@ using UnityEngine.UI;
 
 public class Twinkle : MonoBehaviour
 {
+    private const float addSize=30;
     private float alpha;
-    private const float addSize=0.03f;
-    private float timer;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,27 +16,46 @@ public class Twinkle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(GameManager.BattleState.command!=GameManager.state)
+
+        if(rand==0)
         {
-            Destroy(this.gameObject);
+            f += 10;
         }
+        else
+        {
+            f-=10;
+        }
+        this.transform.Rotate(0, 0, f);
     }
 
-    
+    int f;
+    int rand;
     private IEnumerator Effect()
     {
-        alpha=1;
-        while(timer<0.2f)
+        alpha=0;
+        bool alphaFlag=false;
+       rand= Random.Range(0,2);
+        Vector3 size = this.gameObject.GetComponent<RectTransform>().localScale;
+        while (alpha<1&&!alphaFlag)
         {
-            Vector3 size=this.gameObject.GetComponent<RectTransform>().localScale;
-            size+=new Vector3(addSize,addSize,0);
+            
+            
             Debug.Log(size);
-            alpha-=Time.deltaTime;
-            timer+=Time.deltaTime;
-            this.gameObject.GetComponent<RectTransform>().localScale=size;
+            alpha+=Time.deltaTime*10;
+            this.gameObject.GetComponent<RectTransform>().localScale+=new Vector3(Time.deltaTime,Time.deltaTime,0)*addSize;
             this.gameObject.GetComponent<Image>().color=new Color(1,1,1,alpha);
-            yield return new WaitForSeconds(Time.deltaTime);
+            yield return null;
         }
-        Destroy(this.gameObject);
+       alphaFlag=true;
+        while(alphaFlag&&alpha>0)
+        {
+            Debug.Log(size);
+            alpha -= Time.deltaTime*10;
+            this.gameObject.GetComponent<RectTransform>().localScale += new Vector3(Time.deltaTime, Time.deltaTime, 0)*addSize;
+            this.gameObject.GetComponent<Image>().color = new Color(1, 1, 1, alpha);
+            yield return null;
+        }
+        //  this.gameObject.GetComponent<RectTransform>().localScale=size;
+        //Destroy(this.gameObject);
     }
 }
